12ec6de29d70921cb8f5c4bb49788679b71d63ce
Ivailo Zhelev <Ivailo.zhelev@execview.co.uk>
2016-05-19 21:33:13 +0100

    feat: added the ability to delete nodes that are not part of the files

--- GraphPathFinder/GraphPathFinder.UnitTests/NodeImportService.cs
+++ GraphPathFinder/GraphPathFinder.UnitTests/NodeImportService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using GraphPathFinder.Models;
 using Moq;
 using NUnit.Framework;
@@ -9,11 +10,26 @@ namespace GraphPathFinder.UnitTests
     [TestFixture]
     public class NodeImportServiceTestFixtures
     {
-      
+
+        private Mock<INodeRepository> _nodeRepository;
+
+        [SetUp]
+        public void Setup()
+        {
+             _nodeRepository = new Mock<INodeRepository>();
+        }
+
+
+        private NodeImportService GetNodeImporterInstance(INodeRepository nodeRepository)
+        {
+            return new NodeImportService(nodeRepository);
+        }
+
+        //If new node
         [TestCase]
         public void GivenANewNodeInsertItIntoTheDataStore()
         {
-            var nodeRepository = new Mock<INodeRepository>();
+            var nodeImportService = GetNodeImporterInstance(_nodeRepository.Object);
 
             var newNode = new Node()
             {
@@ -22,21 +38,32 @@ namespace GraphPathFinder.UnitTests
                 Label = "New Node"
             };
 
-            var nodeImportService = new NodeImportService(nodeRepository.Object);
-            nodeImportService.Import(new List<Node>(){newNode});
 
-            nodeRepository.Verify(f=> f.Insert(newNode), Times.Once);      
+            nodeImportService.Import(new List<Node>() { newNode });
+
+            _nodeRepository.Verify(f => f.Insert(newNode), Times.Once);      
         }
 
         //If deleted remove
+         [TestCase]
         public void IfANodeDoesntAppearInTheListDeleteFromDataStore()
         {
-            
+
+            var nodeToDelete = new Node() {Id = 1};
+            var nodeImportService = GetNodeImporterInstance(_nodeRepository.Object);
+
+            _nodeRepository.Setup(f => f.GetAll()).Returns(new List<Node>(){nodeToDelete});
+
+            nodeImportService.Import(new List<Node>() {});
+
+            _nodeRepository.Verify(f => f.Delete(nodeToDelete.Id), Times.Once);  
         }
 
 
 
-        //If update update
+        //If update update - not clear if this is a requirement so leaving this for now
+         
+
     }
 
     public interface INodeRepository
@@ -55,17 +82,27 @@ namespace GraphPathFinder.UnitTests
         {
             _nodeRepository = nodeRepository;
         }
-        public void Import(List<Node> nodes)
+        public void Import(List<Node> newNodes)
         {
             var existingNodes = _nodeRepository.GetAll().ToList();
 
-            foreach (var node in nodes)
+            foreach (var node in newNodes)
             {
                 //With a larger list will be slow. So replace this with a dictionary
                 if (existingNodes.All(f => f.Id != node.Id))
                 {
                     _nodeRepository.Insert(node);
                 }
+            }
+
+            DeleteNodesThatDontExistAnymore(existingNodes, newNodes);
+        }
+
+        private void DeleteNodesThatDontExistAnymore(IEnumerable<Node> existingNodes, IEnumerable<Node> newNode)
+        {
+            foreach (var nodeToDelete in existingNodes.Where(en => newNode.All(nn => nn.Id != en.Id)))
+            {
+                _nodeRepository.Delete(nodeToDelete.Id);
             }
         }
     }
